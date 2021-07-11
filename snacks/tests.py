3c356c9edeeccc25faa46cca099b1ca122886abb
NiveenALSmadi <niveensmadi33@gmail.com>
2021-07-11 15:01:09 +0300

    finish lab26

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class SnacksTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        url = reverse('home')
+        actual_status_code = self.client.get(url).status_code
+        self.assertEqual(actual_status_code, 200)
+
+
+    def test_home_page_templete(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'home.html')
+
+
+    def test_about_page_status_code(self):
+        url = reverse('about')
+        actual_status_code = self.client.get(url).status_code
+        self.assertEqual(actual_status_code, 200)
+
+
+    def test_about_page_templete(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'about.html')
+
+
+       
